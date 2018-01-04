8eb035c5343bec2f794a9cf8e8c920f68ac828df
Sarah Read <sarah@nautilus.hitronhub.home>
2018-01-04 12:13:41 -0700

    Basic login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  username: string;
+  password: string;
+  result: any;
 
-  constructor() { }
-
+  constructor(private http: HttpClient) { }
+  
   ngOnInit() {
+  }
+
+  async login() {
+    this.result = await this.http.post('auth/login', {
+      username: this.username,
+      password: this.password
+    }).toPromise();
   }
 
 }
