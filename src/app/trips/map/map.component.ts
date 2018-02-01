0ab721ae3033a2ff5fb9b78151cccc6e14f3c6b5
Will Blanchette <wb@wblanchette.com>
2018-01-31 22:35:24 -0700

    Refactored code to support multiple routes

--- src/app/trips/map/map.component.ts
+++ src/app/trips/map/map.component.ts
@@ -4,6 +4,11 @@ import { GoogleMapsAPIWrapper, PolylineManager, AgmPolyline, Quer } from '@agm/c
 
 declare var google: any;
 
+interface LatLng {
+  lat: number,
+  lng: number
+}
+
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
@@ -11,14 +16,46 @@ declare var google: any;
   providers: [GoogleMapsAPIWrapper, PolylineManager]
 })
 export class MapComponent implements OnInit {
-    origin = { lng: 4.333, lat: -1.2222 };
-    destination = { lng: 22.311, lat: -0.123 };
+    routes: {
+      origin: { query: string, lat?: number, lng?: number },
+      destination: { query: string, lat?: number, lng?: number },
+      path?: any,
+      finishedPath?: any,
+      percent?: number,
+      totalDistance?: number
+    }[] = [
+      {
+        origin: {
+          query: 'Calgary'
+        },
+        destination: {
+          query: 'Edmonton'
+        },
+        percent: 0.6
+      },
+      {
+        origin: {
+          query: 'Barcelona, Spain'
+        },
+        destination: {
+          query: 'Madrid'
+        },
+        percent: 0.85
+      },
+      {
+        origin: {
+          query: 'Frankfurt, Germany'
+        },
+        destination: {
+          query: 'Venice, Italy'
+        },
+        percent: 0.3
+      }
+    ];
 
     directionsService;
+    placesService;
     map;
-    path;
-    finishedPath;
-    percent = 0.5;
 
     styles = [
       {
@@ -264,75 +301,78 @@ export class MapComponent implements OnInit {
 
     ngAfterViewInit() {}
 
-    onMapReady(map) {
+    async onMapReady(map) {
       this.map = map;
 
       this.directionsService = new google.maps.DirectionsService;
-      const placesService = new google.maps.places.PlacesService(map);
+      this.placesService = new google.maps.places.PlacesService(map);
       
-      placesService.textSearch({
-        query: 'Calgary'
-      }, (response, status) => {
-        if (status == 'OK') {
-          this.origin = response[0].geometry.location;
-          this.updateMap();
-        } else {
-          // TODO
-        }
-      });
+      // Fetch lat / lng / path for routes
 
-      placesService.textSearch({
-        query: 'Edmonton'
-      }, (response, status) => {
-        if (status == 'OK') {
-          this.destination = response[0].geometry.location;
-          this.updateMap();
-        } else {
-          // TODO
-        }
-      });
+      for (let ii = 0; ii < this.routes.length; ii++) {
+        const route = this.routes[ii];
+        const origin = await this.searchLocation(route.origin.query);
+        const destination = await this.searchLocation(route.destination.query);
+
+        Object.assign(route.origin, origin);
+        Object.assign(route.destination, destination);
+
+        [route.path, route.totalDistance] = await new Promise<any>(resolve => {
+          this.directionsService.route({
+            origin,
+            destination,
+            waypoints: [],
+            optimizeWaypoints: true,
+            travelMode: 'DRIVING'
+          }, (response, status) => {
+            if (status == 'OK') {
+              const path = response.routes[0].overview_path;
+
+              // Calculate total length of path
+
+              const totalDistance = response.routes[0].legs.reduce((a, b) => {
+                return a ? a.distance.value: 0 + b ? b.distance.value : 0;
+              }, 0);
+
+              resolve([path, totalDistance]);
+            } else {
+              console.log('error searching route');
+            }
+          });
+        });
+      }
+
+      this.updateMap();
+    }
+
+    async searchLocation(query: string): Promise<any>{
+      return new Promise(resolve => {
+          this.placesService.textSearch({ query }, (response, status) => {
+            if (status == 'OK') {
+              resolve(response[0].geometry.location);
+            } else {
+              console.log('error searching location:', query);
+            }
+          });
+        });
     }
 
     updateMap() {
-      this.directionsService.route({
-        origin: this.origin,
-        destination: this.destination,
-        waypoints: [],
-        optimizeWaypoints: true,
-        travelMode: 'DRIVING'
-      }, (response, status) => {
-        if (status == 'OK') {
-          console.log('response', response);
-          this.path = response.routes[0].overview_path;
+      this.routes.forEach(route => {
+        const targetDistance = route.totalDistance * route.percent;
+        let distance = 0;
 
-          // Calculate total length of path
+        for (let ii = 0; ii < route.path.length - 1; ii++) {
+          
+          distance += google.maps.geometry.spherical.computeDistanceBetween(
+            route.path[ii],
+            route.path[ii + 1]
+          );
 
-          const totalDistance = response.routes[0].legs.reduce((a, b) => {
-            return a ? a.distance.value: 0 + b ? b.distance.value : 0;
-          }, 0);
-
-          console.log('distance', totalDistance);
-
-          // Calculate point along path
-
-          const targetDistance = totalDistance * this.percent;
-          let distance = 0;
-
-          for (let ii = 0; ii < this.path.length; ii++) {
-            distance += google.maps.geometry.spherical.computeDistanceBetween(
-              this.path[ii],
-              this.path[ii + 1]
-            );
-
-            if (distance >= targetDistance) {
-              console.log('hit distance', targetDistance, 'at', ii);
-              this.finishedPath = this.path.slice(0, ii - 1);
-              break;
-            }
+          if (distance >= targetDistance) {
+            route.finishedPath = route.path.slice(0, ii - 1);
+            break;
           }
-
-        } else {
-          console.log('failed', status, response);
         }
       });
     }
